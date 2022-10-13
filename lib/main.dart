1880803ae42ce79f25e37a6dec03d8487727fdf1
Edwin Murari <edwin.murari@myxplor.com>
2022-10-13 14:39:56 +1100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:contact/src/my_app.dart';
+import 'package:edu_comms_home/src/my_app.dart';
+
+@pragma('vm:entry-point')
+void contactsModule() => runApp(const ContactsApp());
+
+@pragma('vm:entry-point')
+void eduCommsModule() => runApp(const EduCommsApp());
